626eb70c9d00dc2b89777e15893087e29cf4d8e9
Elma Nevala <elma.nevala@helsinki.fi>
2020-11-06 19:23:40 +0200

    checkstylen rikkominen

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -56,7 +56,7 @@ public class Varasto {
     public void lisaaVarastoon(double maara) {
         if (maara < 0){ // virhetilanteessa voidaan tehdä 
             return;       // tällainen pikapoistuminenkin!
-        }
+    }
         if (maara <= paljonkoMahtuu()){ // omia aksessoreita voi kutsua
             saldo = saldo + maara;          // ihan suoraan sellaisinaan
         } else {
